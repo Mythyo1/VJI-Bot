9d282003d8a8c4506014f3b08e71e0775b046ddc
Syn <45761547+SynCode05@users.noreply.github.com>
2020-11-01 18:16:09 +0500

    Update

--- modules/onDatabase.js
+++ modules/onDatabase.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const { readdir } = require('fs')
 
 module.exports.run = (client) => {
     // Connect to the DB
@@ -6,8 +7,19 @@ module.exports.run = (client) => {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => {
-        client.logger.ready('Connected to the database')
+        client.logger.ready('Connected to the database');
+        readdir('models', async (err, files) => {
+            if (err) return client.logger.error(err);
+            for (let i = 0; i < files.length; i++) {
+
+                let model = require('models/' + files[i]);
+                let docs = await model.find({});
+                for (let x = 0; x < docs.length; x++) {
+                    await docs[x].save();
+                };
+            };
+        });
     }).catch((err) => {
-        client.logger.error(err)
+        client.logger.error(err);
     });
 }
